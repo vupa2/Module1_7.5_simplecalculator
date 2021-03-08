3cdc596ff503149a733e2233c464ec1157e7424c
vupa2 <anhvu.pa96@gmail.com>
2021-03-08 15:08:32 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const myMath = (id) => {
+  const left = parseFloat(document.querySelector('#left').value);
+  const right = parseFloat(document.querySelector('#right').value);
+
+  switch (id) {
+    case 'plus':
+      return left + right;
+      break;
+    case 'subtract':
+      return left - right;
+      break;
+    case 'mutiply':
+      return left * right;
+      break;
+    case 'devide':
+      return left / right;
+      break;
+    default :
+      return 0;
+  }
+}
+
+document.querySelectorAll('div > button').forEach(button => button.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  document.querySelector('#result').innerHTML = `Results: ${myMath(e.target.id)}`;
+}))
